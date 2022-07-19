6b119cd7ff681662fec9b12a9f4cd469b6ef6c70
Chandelure51 <chandelure091@gmail.com>
2022-07-19 18:06:10 +0900

    [add]developersファイルの作成

--- database/migrations/2014_10_12_000000_create_developers_table.php
+++ database/migrations/2014_10_12_000000_create_developers_table.php
@@ -13,12 +13,15 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('developers', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+	        $table->string('name');
+	        $table->enum('gender',['man','woman','others']);
+	        $table->string('email')->unique();
+        	$table->timestamp('email_verified_at')->nullable();
+        	$table->string('developer_avatar');
+        	$table->string('password');
+        	$table->integer('age')->nullable;
             $table->rememberToken();
             $table->timestamps();
         });
@@ -31,6 +34,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('developers');
     }
 };
